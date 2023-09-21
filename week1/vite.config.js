dd60982d32cae90be6f2eaee8aad1e6c91efa44d
panduola666 <panduola666@gmail.com>
2023-09-22 00:47:49 +0800

    [feat] 建立環境

---
+++ week1/vite.config.js
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? '/2023_F2E/'
+    : '/',
+  plugins: [
+    vue()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
